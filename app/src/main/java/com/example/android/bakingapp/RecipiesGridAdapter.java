381a3c5162733f93afcdce31b84166dffff4f139
Adesire <desilere@gmail.com>
2018-11-10 15:13:38 +0100

    1st Commit

--- app/src/main/java/com/example/android/bakingapp/RecipiesGridAdapter.java
+++ app/src/main/java/com/example/android/bakingapp/RecipiesGridAdapter.java
@@ -22,9 +22,6 @@ public class RecipiesGridAdapter extends RecyclerView.Adapter<RecipiesGridAdapte
     private Context mContext;
     private JSONArray mJSONArray;
     final private GridItemClickListener mListener;
-    static ArrayList<RecipeModel> recipeList = new ArrayList<>();
-    ArrayList<IngredientsFragment> mineIng = new ArrayList<>();
-    ArrayList<Steps> mineSteps = new ArrayList<>();
     public static String RECIPE_OBJECT = "Recipe Object";
 
     public RecipiesGridAdapter(Context mContext, JSONArray mJSONArray,GridItemClickListener listener){
@@ -58,14 +55,7 @@ public class RecipiesGridAdapter extends RecyclerView.Adapter<RecipiesGridAdapte
         int length = 0;
         if (mJSONArray == null)
             return length;
-        /*try {
 
-            length = mJSONArray.length();
-
-        } catch (JSONException e) {
-            Log.e("TAG", e.getLocalizedMessage());
-            e.printStackTrace();
-        }*/
         return mJSONArray.length();
     }
 
@@ -85,63 +75,19 @@ public class RecipiesGridAdapter extends RecyclerView.Adapter<RecipiesGridAdapte
 
         void bind(int itemPosition){
             //JSONObject recipe = null;
-            RecipeModel model = new RecipeModel();
             try {
 
                 JSONObject recipe = mJSONArray.getJSONObject(itemPosition);
                 int id = recipe.getInt("id");
                 String name = recipe.getString("name");
-                model.setId(id);
-                model.setName(name);
 
-                JSONArray ingredients = recipe.getJSONArray("ingredients");
-                for(int i=0;i<=ingredients.length();i++){
-                    JSONObject object = ingredients.getJSONObject(i);
-                    int quantity = object.getInt("quantity");
-                    String measure = object.getString("measure");
-                    String ingredient = object.getString("ingredient");
-
-                    /*Log.e("TAGGG",ingredient);
-                    model.setQuantity(quantity);
-                    model.setMeasure(measure);
-                    model.setIngredient(ingredient);*/
-
-                    //mineIng.add(new IngredientsFragment(quantity,measure,ingredient));
-                    model.setMyIng(mineIng);
-                }
-
-                JSONArray steps = recipe.getJSONArray("steps");
-                for(int i=0;i<=steps.length();i++){
-                    JSONObject object = steps.getJSONObject(i);
-                    int step_id = object.getInt("id");
-                    String shortDescription = object.getString("shortDescription");
-                    String description = object.getString("description");
-                    String video = object.getString("videoURL");
-                    String thumbnail = object.getString("thumbnail");
-
-                    /*model.setStep_id(step_id);
-                    model.setShortDesc(shortDescription);
-                    model.setDec(description);
-                    model.setVideo(video);
-                    model.setThumbnail(thumbnail);*/
-                    mineSteps.add(new Steps(step_id,shortDescription,description,video));
-                    model.setMySteps(mineSteps);
-
-                }
                 int servings = recipe.getInt("servings");
                 String image = recipe.getString("image");
 
-                model.setServings(servings);
-                model.setImage(image);
-
+                title.setText(name);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            recipeList.add(model);
-            title.setText(model.getName());
-
-
-
         }
 
         @Override

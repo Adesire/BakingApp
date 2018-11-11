100a6c5f1ff1b17dec80b2ae64cd74b326bc2db8
oluwatayo <oluwatayoadegboye@gmail.com>
2018-11-12 00:33:44 +0100

    add widget

--- app/src/main/java/com/example/android/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapp/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.android.bakingapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -21,6 +27,9 @@ public class MainActivity extends AppCompatActivity implements RecipiesGridAdapt
     private static final String URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json" ;
     GridLayoutManager mLayoutManager;
     RecyclerView recipeGrid;
+    private SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    public static final String PREF_DATA = "pref_data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +39,18 @@ public class MainActivity extends AppCompatActivity implements RecipiesGridAdapt
         recipeGrid = (RecyclerView)findViewById(R.id.recipe_grid);
         mLayoutManager = new GridLayoutManager(MainActivity.this,2,GridLayoutManager.VERTICAL,false);
         recipeGrid.setLayoutManager(mLayoutManager);
-
-        networkingProtocols();
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!sharedPreferences.contains(PREF_DATA))
+            networkingProtocols();
+        else {
+            try {
+                showData(new JSONArray(sharedPreferences.getString(PREF_DATA, "")));
+                Log.e("ddd", sharedPreferences.getString("4", "hh"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+                networkingProtocols();
+            }
+        }
     }
 
     @Override
@@ -47,8 +66,15 @@ public class MainActivity extends AppCompatActivity implements RecipiesGridAdapt
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 super.onSuccess(statusCode, headers, response);
-                recipeGrid.setAdapter(new RecipiesGridAdapter(MainActivity.this,response,
-                        MainActivity.this));
+                editor = sharedPreferences.edit();
+                editor.putString(PREF_DATA, response.toString());
+                for (int i = 0; i < response.length(); i++){
+                    JSONObject o = response.optJSONObject(i);
+                    String ing = o.optJSONArray("ingredients").toString();
+                    editor.putString(String.valueOf(i+1), ing);
+                }
+                editor.apply();
+                showData(response);
             }
 
             @Override
@@ -61,5 +87,24 @@ public class MainActivity extends AppCompatActivity implements RecipiesGridAdapt
                 super.onRetry(retryNo);
             }
         });
+    }
+
+    private void showData(JSONArray data){
+        recipeGrid.setAdapter(new RecipiesGridAdapter(MainActivity.this,data,
+                MainActivity.this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 1, 1, "Widget");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == 1){
+            startActivity(new Intent(this, SettingsActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

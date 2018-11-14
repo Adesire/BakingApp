06423fe5de2392be2b1c6bbe360884fbb8129763
Adesire <desilere@gmail.com>
2018-11-14 17:51:13 +0100

    Test

--- app/src/androidTest/java/com/example/android/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/android/bakingapp/MainActivityTest.java
@@ -6,11 +6,13 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.anything;
 
 import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -19,7 +21,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.espresso.contrib.RecyclerViewActions;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -33,10 +34,7 @@ public class MainActivityTest {
         //RECYCLERVIEW
         //MY PLAN WAS TO TEST THE RECYCLER  VIEW, AND THE BUTTON CLICKS OF THE RECIPE DETAILS FRAGMENT
         //WHICH OPENS THE RECIPE INGREDIENTS.
-        /*onView(ViewMatchers.withId(R.id.recipe_grid))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(0, ViewAction.l));*/
-
-        /*onView(withId(R.id.recipe_grid))
-                .perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Nutella Pie")),click()));*/
+        onView(withId(R.id.recipe_grid))
+                .check(matches(isDisplayed()));
     }
 }
